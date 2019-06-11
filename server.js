810555fe437dbb22e77a7e364e6a63e064c0428f
Your Name <you@example.com>
2019-06-11 23:20:16 +0400

    1st step

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+app.use(express.static("."));
+app.get('/', function (req, res) {
+    res.redirect('index.html');
+});
+
+server.listen(2000);
+io.on("connection", function (socket) {
+
+
+    
+})
